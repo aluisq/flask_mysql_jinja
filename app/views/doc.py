5745e2cffdb44ff21350c69ce50866ff5d078927
Arthur Luis Dantas de Siqueira <arthur.dantas@unimedrecife.com.br>
2020-08-24 16:58:36 -0300

    page to pdf and documents

--- app/views/doc.py
+++ app/views/doc.py
@@ -4,3 +4,7 @@ from flask import render_template, request, redirect, url_for
 @app.route('/doc')
 def doc():
     return render_template('public/doc.html')
+
+@app.route('/doc/teste')
+def doc_pdf():
+    return send_file('static/pdf/teste.pdf')

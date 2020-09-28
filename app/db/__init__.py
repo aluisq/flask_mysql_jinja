a979f8c8d7a0579cc22c9cd2763e5f945ac68ef0
aluisq <arthurl.dan@gmail.com>
2020-09-28 10:18:13 -0300

    Added feature to connect with flask login

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -6,21 +6,13 @@ try:
     connection = mysql.connector.connect(host= os.environ.get("HOST"),
                                          database= os.environ.get("DATABASE"),
                                          user= os.environ.get("USER"),
-                                         password= os.environ.get("PASSWORD"), connection_timeout= 180)
+                                         password= os.environ.get("PASSWORD"))
 
     if connection.is_connected():
             db_Info = connection.get_server_info()
-            global_connect_timeout = 'SET GLOBAL connect_timeout=180'
-            global_wait_timeout = 'SET GLOBAL connect_timeout=180'
-            global_interactive_timeout = 'SET GLOBAL connect_timeout=180'
+            
             print("Connected to MySQL Server version", db_Info)
             cursor = connection.cursor()
-            cursor.execute(global_connect_timeout)
-            cursor.execute(global_wait_timeout)
-            cursor.execute(global_interactive_timeout)
        
-            connection.commit()
-
-
 except Error as e:
     print("Error while connecting to MySQL: ", e)
